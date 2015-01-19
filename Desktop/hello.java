54e26748f2b6f9868f1765c844d017a0540bf4f5
abhi <abhinaythurlapati@gmail.com>
2015-01-19 13:52:42 +0530

    Added hello world program in java

---
+++ Desktop/hello.java
@@ -0,0 +1,13 @@
+class Bike7{
+    int speed;
+    
+    
+ 
+    {speed=100;system.out.println("hello world");}
+     
+    public static void main(String args[]){
+    Bike7 b1=new Bike7();
+    Bike7 b2=new Bike7();
+    }    
+}
+
